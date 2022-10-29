f7f51f2187542abe1cbd5c2e072e8be9593cd0e6
chi_duong <anhchictw311@gmail.com>
2022-10-29 14:16:26 +0700

    add ssl

--- src/index.js
+++ src/index.js
@@ -9,15 +9,22 @@ import { InitiateMongoServer } from './api/v1/datasources/index.js'
 // import connectRedis from 'connect-redis'
 // import client from './api/helpers/connection_redis.js'
 import route from './api/v1/routes/index.js'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 InitiateMongoServer()
 
 const port = 3001
 
 app.use(cors())
+// app.use(express.static('./public'))
+app.use(express.static( __dirname + '/public'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
     res.send(`success`)
